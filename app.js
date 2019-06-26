fbb81cec20dc1afc4602c3f4206cde02e71d7038
Barnaby <barnabywaweru@hotmail.com>
2019-06-26 10:36:47 +0300

    [finishes #166857501]added routes to app.js

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 /* eslint-disable indent */
 import dotenv from 'dotenv';
 import express from 'express';
+import '@babel/polyfill';
+import bodyParser from 'body-parser';
+import routes from './server/routes';
 
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true,
+}));
 
+routes(app);
 dotenv.config();
 
 app.get('/', (req, res) => {
