64439bfe0c98f4a2ab126f0403870f00136231ae
Barnaby <barnabywaweru@hotmail.com>
2019-06-26 10:35:43 +0300

    [feature #166857501]created signup model

---
+++ server/models/userModel.js
@@ -0,0 +1,32 @@
+import db from '../db/users';
+
+class UserModel {
+  constructor(payload = null) {
+    this.payload = payload;
+    this.result = null;
+  }
+
+  async registerUser() {
+    const user = {
+      _id: this.payload._id,
+      email: this.payload.email,
+      firstname: this.payload.firstname,
+      lastname: this.payload.lastname,
+      password: this.payload.password,
+      phoneNumber: this.payload.phoneNumber,
+      address: this.payload.address,
+      is_Agent: this.payload.is_Agent,
+      is_Admin: this.payload.is_Admin,
+    };
+
+    const obj = db.find(o => o.email === this.payload.email);
+    if (!obj) {
+      db.push(user);
+      this.result = user;
+      return true;
+    }
+    return false;
+  }
+}
+
+export default UserModel;
