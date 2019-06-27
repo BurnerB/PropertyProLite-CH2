4f16d94d1dafd932436fe9283ecc719aca53612f
Barnaby <barnabywaweru@hotmail.com>
2019-06-27 09:40:29 +0300

    [finishes #166857795] added login route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,7 @@ import Validation from '../middleware/validation';
 const router = express.Router();
 
 
-router.post('/auth/signup',Validation.validateSignup, Users.registerUser);
+router.post('/auth/signup', Validation.validateSignup, Users.registerUser);
+router.post('/login', Users.userLogin);
 
 export default router;
