bad52dbded1ae07fbcb7f5018adef17221d3f533
Barnaby <barnabywaweru@hotmail.com>
2019-06-28 13:03:57 +0300

    [feature #166858131] create property endpoints

---
+++ server/controllers/propertyEndpoints.js
@@ -0,0 +1,51 @@
+import moment from 'moment';
+import PropertyModel from '../models/propertyModel';
+import db from '../db/adverts';
+
+// eslint-disable-next-line camelcase
+const created_on = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+class Property {
+  static async postProperty(req, res) {
+    try {
+      const {
+        status,
+        type,
+        state,
+        city,
+        price,
+        address,
+        image_url,
+      } = req.body;
+
+      const _id = db.length + 1;
+      const owner = 1;
+
+      const newAdvert = new PropertyModel({
+        _id,
+        owner,
+        status,
+        type,
+        state,
+        city,
+        price,
+        address,
+        created_on,
+        image_url,
+      });
+      
+      await newAdvert.registerProperty()
+      res.status(201)
+        .json({
+          status: 'success',
+          data: newAdvert.result,
+        });
+      return;
+    } catch (e) {
+      console.log(e);
+      res.status(500);
+    }
+  }
+};
+
+export default Property;
