a75f3ec1c84e2b1a14f8e759b3e6f0973a461f56
Barnaby <barnabywaweru@hotmail.com>
2019-07-01 08:47:58 +0300

    [feature #167002694]added admin user to db

--- server/db/users.js
+++ server/db/users.js
@@ -1,4 +1,16 @@
-const users = [];
+const users = [
+  {
+    _id: 1,
+    email: 'agent007@gmail.com',
+    firstname: 'James',
+    lastname: 'Bond',
+    password: '$2b$10$MQiD7iV.aPTIeOwq5PVDI.E62HP35STcEl10c/Z/6y/rt5u705gha',
+    phoneNumber: '0700000000',
+    address: 'Rwanda',
+    is_Agent: true,
+    is_Admin: true,
+  },
+];
 
 
 export default users;
