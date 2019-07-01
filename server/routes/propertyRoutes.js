50aa61de7efbecad7ae0bb3a61262874a79b3e8b
Barnaby <barnabywaweru@hotmail.com>
2019-07-01 08:46:03 +0300

    [feature #167002694]add new middleware toutes

--- server/routes/propertyRoutes.js
+++ server/routes/propertyRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import Property from '../controllers/propertyEndpoints';
 import Validation from '../middleware/validation';
+import auth from '../middleware/auth';
+import agent from '../middleware/agent';
 
 const router = express.Router();
 
-router.post('/property', Validation.validateProperty, Property.postProperty);
+router.post('/property', [auth, agent], Validation.validateProperty, Property.postProperty);
 
 export default router;
