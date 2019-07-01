1bf8ebcbe44470e4616d45c37d1ccef288affbb5
Barnaby <barnabywaweru@hotmail.com>
2019-07-01 20:38:35 +0300

    [feature #166858175]added update model

--- server/models/propertyModel.js
+++ server/models/propertyModel.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import db from '../db/adverts';
 
 class PropertyModel {
@@ -6,7 +7,7 @@ class PropertyModel {
     this.result = null;
   }
 
-  async registerProperty() {
+  async createProperty() {
     const property = {
       _id: this.payload._id,
       owner: this.payload.owner,
@@ -21,6 +22,27 @@ class PropertyModel {
     };
     db.push(property);
     this.result = property;
+    return true;
+  }
+
+  async updateProperty() {
+    const obj = db.find(o => o._id === parseInt(this.payload._id));
+    if (!obj) {
+      return false;
+    }
+    const newAdvert = {
+      _id: obj._id,
+      owner: obj.owner,
+      status: obj.status,
+      type: this.payload.type,
+      state: this.payload.state,
+      city: this.payload.city,
+      price: this.payload.price,
+      address: this.payload.address,
+      image_url: this.payload.image_url,
+    };
+    db.splice(obj._id - 1, 1, newAdvert);
+    this.result = newAdvert;
     return true;
   }
 }
