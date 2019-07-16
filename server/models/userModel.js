95c1550ffb61788d8a182713ea7ff249783dd9de
Barnaby <barnabywaweru@hotmail.com>
2019-07-16 22:19:48 +0300

    [feature #167314882]modified usermodels

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,29 +2,30 @@
 import db from '../db/queries';
 
 class UserModel {
-  constructor(firstname, lastname, email, password, address, is_Agent, is_Admin) {
+  constructor(firstname, lastname, email, password, address, is_Agent) {
     this.firstname = firstname;
     this.lastname = lastname;
     this.email = email;
     this.password = password;
     this.address = address;
     this.is_Agent = is_Agent;
-    this.is_Admin = is_Admin;
+    this.is_Admin = false;
 
   }
 
   async registerUser() {
-    const createUserQuery = `INSERT INTO users(firstname,lastname,email,password, address,is_Agent, is_Admin) VALUES($1, $2, $3, $4, $5 ,$6, $7) returning *`;
-    const value = [this.firstname,this.lastname,this.email,this.password,this.address,this.is_Agent, this.is_Admin];
+    const createUserQuery = `INSERT INTO users ( firstname, lastname, email, password, address, is_Agent, is_Admin) VALUES($1, $2, $3, $4, $5 ,$6, $7) returning *`;
+    const value = [this.firstname, this.lastname, this.email,  this.password, this.address, this.is_Agent, this.is_Admin];
     const createdUser = await db.query(createUserQuery,value);
-    const user = createdUser.rows[0];
+    const user = createdUser[0].firstname;
+    console.log(createdUser);
     return user;
   }
 
-  // async findbyEmail(email) {
-  //   const searchUserQuery = `SELECT * FROM users WHERE email= $1`;
-  //   const foundUser = await db.query(searchUserQuery,[email])
-  //   return foundUser.row[0];
-  // }
+  static async findbyEmail(email) {
+    const searchUserQuery = `SELECT * FROM users WHERE email= $1`;
+    const foundUser = await db.query(searchUserQuery,[email])
+    return foundUser.rows[0];
+  }
 }
 export default UserModel;
