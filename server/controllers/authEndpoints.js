0eb9db59050e48e87bca7a8358be1233e6800ce2
Barnaby <barnabywaweru@hotmail.com>
2019-07-17 11:52:42 +0300

    [feature #167287805]modify login endpoint methods

--- server/controllers/authEndpoints.js
+++ server/controllers/authEndpoints.js
@@ -20,9 +20,9 @@ class Authentication {
         is_Agent,
       } = req.body;
 
-      const user = await UserModel.findbyEmail(email);
+      const user1 = await UserModel.findbyEmail(email);
 
-      if(!user){
+      if(!user1){
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
         const newUser = new UserModel(
@@ -36,20 +36,11 @@ class Authentication {
         const user = await newUser.registerUser();
 
         const token = Token.genToken(user.id, user.email, user.firstname, user.lastname, user.is_Agent);
+        user["token"]=token;
 
-        return res.status(201).json({
-          status: 'success',
-          data: {
-            token: token,
-            firstName: user[0].firstname,
-            lastName: user[0].lastname,
-            email: user[0].email,
-            id: user[0].id,
-            address:user[0].address
-
-            },
-          });
-      }
+        return response.authsuccess(201, 'successfully created user', 
+                                        _.pick(user,['id','email', 'firstname','lastname','phoneNumber','address', 'is_Admin','is_Agent','token']), res);
+        }
       return response.handleError(409, 'The email has already been used to register', res);
     
     } catch (e) {
@@ -60,20 +51,14 @@ class Authentication {
   static async userLogin(req, res) {
 		try {
 			const { email, password } = req.body;
-			const user = new UserModel(email);
+			const user = await UserModel.findbyEmail(email);
 
-			if (await user.findbyEmail()) {
-  
-        const {
-            // eslint-disable-next-line no-shadow
-            _id, firstname, lastname, is_Admin, is_Agent, phoneNumber,
-        } = user.result;
-
-				if (bcrypt.compareSync(password, user.result.password)) {
-            const token = Token.genToken(_id, email, firstname, lastname, is_Agent, is_Admin, phoneNumber);
-            user.result["token"]=token;
-            return response.authsuccess(200, 'success', 
-                                        _.pick(user.result,['_id','email', 'firstname','lastname','phoneNumber','address', 'is_Admin','is_Agent','token']), res);
+			if (user) {
+				if (bcrypt.compareSync(password, user.password)) {
+            const token = Token.genToken(user.id, user.email, user.firstname, user.lastname, user.is_Agent);
+            user["token"]=token;
+            return response.authsuccess(200, 'successly logged in', 
+                                        _.pick(user,['id','email', 'firstname','lastname','phoneNumber','address', 'is_Admin','is_Agent','token']), res);
         }
         return response.handleError(401, 'Incorrect password Email combination', res);
 
@@ -83,19 +68,19 @@ class Authentication {
 		}
   }
 
-  static async resetPassword(req, res) {
-		try {
-      const { email, phoneNumber } = req.body;
+  // static async resetPassword(req, res) {
+	// 	try {
+  //     const { email, phoneNumber } = req.body;
 
-      const user = new UserModel(email);
-      if (!await user.findbyEmail()) {
-        return response.handleError(404, 'No account with this email found', res);
+  //     const user = new UserModel(email);
+  //     if (!await user.findbyEmail()) {
+  //       return response.handleError(404, 'No account with this email found', res);
 
-      } return response.success(200, 'Details on password reset have been sent to your email address', res);
-		} catch (e) {
-      return response.catchError(500, e.toString(), res);
-		}
-  }
+  //     } return response.success(200, 'Details on password reset have been sent to your email address', res);
+	// 	} catch (e) {
+  //     return response.catchError(500, e.toString(), res);
+	// 	}
+  // }
 }
 
 export default Authentication;
