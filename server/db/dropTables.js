ee62b02235b7c52d8d8c0d00885260deb999c617
Barnaby <barnabywaweru@hotmail.com>
2019-07-17 10:03:33 +0300

    [feature #167314882]modify drop tables

--- server/db/dropTables.js
+++ server/db/dropTables.js
@@ -1,26 +1,14 @@
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
+const { pool } = require('../../config/config');
 
-dotenv.config();
-const pool = new Pool(
-  {
-    user:process.env.USER,
-    password:'password',
-    Host:process.env.HOST,
-    port:process.env.PORT,
-    database:process.env.DATABASE
+const dropTables = async () => {
+  try{
+    const queryText = 'DROP TABLE IF EXISTS users';
+    await pool.query(queryText);
+    await pool.end();
+  }catch{
+    console.log(err);
+    pool.end();
   }
-);
-
-const dropTables = async() => {
-  const queryText = 'DROP TABLE IF EXISTS users';
-  await pool.query(queryText)
-  await pool.end();
-    }
-    
-pool.on('remove', () => {
-  console.log('client removed');
-  process.exit(0);
-    });
+}
 
 dropTables();
