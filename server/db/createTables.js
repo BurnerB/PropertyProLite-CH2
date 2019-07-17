9b2b23ec268adb2ddac68ed6949d95d384ef76d2
Barnaby <barnabywaweru@hotmail.com>
2019-07-17 10:03:19 +0300

    [feature #167314882]modify create tables

--- server/db/createTables.js
+++ server/db/createTables.js
@@ -1,16 +1,4 @@
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
-
-dotenv.config();
-const pool = new Pool(
-  {
-    user:process.env.USER,
-    password:'password',
-    Host:process.env.HOST,
-    port:process.env.PORT,
-    database:process.env.DATABASE
-  }
-);
+const { pool } = require('../../config/config');
 
 pool.on('connect', () => {
   console.log('connected to the db');
@@ -21,16 +9,15 @@ const createTables = async () => {
     `CREATE TABLE IF NOT EXISTS
       users(
         id serial PRIMARY KEY,
-        firstname VARCHAR(500) ,
-        lastname VARCHAR(500) ,
-        email VARCHAR(500)  UNIQUE ,
+        firstname VARCHAR(500),
+        lastname VARCHAR(500),
+        email VARCHAR(500) ,
         password VARCHAR(500),
         address VARCHAR(500) ,
-        is_Agent BOOL DEFAULT 'false',
-        is_Admin BOOL DEFAULT 'false',
+        is_Agent BOOL DEFAULT false,
+        is_Admin BOOL DEFAULT false,
         created_On TIMESTAMP DEFAULT NOW()
-
-      );`;
+      )`;
 
   await pool.query(queryText)
   await pool.end();
