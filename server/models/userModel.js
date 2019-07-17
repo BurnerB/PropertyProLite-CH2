ce3010c24af2b3738580733e6aa623afb88eee52
Barnaby <barnabywaweru@hotmail.com>
2019-07-17 10:04:41 +0300

    [feature #167314882]modify user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,31 +1,30 @@
 /* eslint-disable no-underscore-dangle */
-import db from '../db/queries';
+import pool from '../../config/config';
 
 class UserModel {
-  constructor(firstname, lastname, email, password, address, is_Agent) {
+  constructor(firstname, lastname, email, password, address, is_Agent, is_Admin) {
     this.firstname = firstname;
     this.lastname = lastname;
     this.email = email;
     this.password = password;
     this.address = address;
     this.is_Agent = is_Agent;
-    this.is_Admin = false;
+    this.is_Admin = is_Admin;
 
   }
 
   async registerUser() {
-    const createUserQuery = `INSERT INTO users ( firstname, lastname, email, password, address, is_Agent, is_Admin) VALUES($1, $2, $3, $4, $5 ,$6, $7) returning *`;
-    const value = [this.firstname, this.lastname, this.email,  this.password, this.address, this.is_Agent, this.is_Admin];
-    const createdUser = await db.query(createUserQuery,value);
-    const user = createdUser[0].firstname;
-    console.log(createdUser);
-    return user;
+    const createUserQuery = `INSERT INTO users(firstname,lastname,email,password, address,is_Agent, is_Admin) VALUES($1, $2, $3, $4, $5 ,$6, $7) returning *`;
+    const createdUser = await pool.query(createUserQuery, [this.firstname,this.lastname,this.email,this.password,this.address,this.is_Agent, this.is_Admin]);
+    const { rows }= createdUser
+    return rows;
   }
 
   static async findbyEmail(email) {
     const searchUserQuery = `SELECT * FROM users WHERE email= $1`;
-    const foundUser = await db.query(searchUserQuery,[email])
-    return foundUser.rows[0];
+    const foundUser = await pool.query(searchUserQuery,[email])
+    const user = foundUser.rows[0];
+    return user;
   }
 }
 export default UserModel;
