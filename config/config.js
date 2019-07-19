8f8c0c31b6b5a2f17e6d950e52a69ba233320380
Barnaby <barnabywaweru@hotmail.com>
2019-07-19 10:31:19 +0300

    fix-heroku

--- config/config.js
+++ config/config.js
@@ -1,29 +1,27 @@
 const { Pool } = require('pg');
 //connection to postgres
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const env = process.env.NODE_ENV;
 
-const database = env ==='test'? process.env.TEST_DATABASE : process.env.DEV_DATABASE;
+// const database = env ==='test'? process.env.TEST_DATABASE : process.env.DEV_DATABASE;
 
 
-const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: database,
-    password: 'password',
-    port: process.env.PORT,
-  });
-// }
-// if(process.env.NODE_ENV = "test"){
-//   pool = new Pool({
+const databaseUrl = process.env.DATABASE_URL
+// const pool = new Pool({
 //     user: process.env.USER,
 //     host: process.env.HOST,
-//     database: process.env.DATABASE,
+//     database: database,
 //     password: 'password',
 //     port: process.env.PORT,
 //   });
-// }
+
+
+const pool = new Pool({
+  connectionString:databaseUrl
+})
 
 
 pool.on('connect', () => console.log('connected to the db'));
